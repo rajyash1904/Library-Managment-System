68eb8defbc116148139923563b927494e7a1a292
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-07-22 22:50:07 +0530

    Project Completed

--- Library Managment System/src/library/managment/system/LibraryManagmentSystem.java
+++ Library Managment System/src/library/managment/system/LibraryManagmentSystem.java
@@ -16,6 +16,7 @@ public class LibraryManagmentSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         new login().setVisible(true);
     }
     
 }
--- Library Managment System/src/project/ConnectionProvider.java
+++ Library Managment System/src/project/ConnectionProvider.java
@@ -17,7 +17,7 @@ public static Connection getcon()
     try
     {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms?characterEncoding=latin1", "root", "root");
+        Connection con=DriverManager.getConnection("jdbc:mysql://bddp3sheu6ovo9it0ma8-mysql.services.clever-cloud.com:3306/bddp3sheu6ovo9it0ma8?characterEncoding=latin1", "ueqcpo4fg2w2osp8", "3BtW9gw1zhQXbLTGR4eD");
         return con;
     }catch(Exception e)
     {
